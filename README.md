7f4bae2b71cca962038834b4f2fb73e9031185d8
Cathal Corbett <cathalcorbett3@gmail.com>
2020-07-08 10:28:06 +0100

    Pip Example Package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Package
+
+This is a simple example python pip package. You can use [Github-flavoured Markdown](https://guides.github.com/features/mastering-markdown/) to write your content.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip_package_cathal",
+    version="0.0.2",
+    author="Cathal Corbett",
+    author_email="cathalcorbett3@gmail.com",
+    description="Example Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ccathal/pip_package_example",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating SYstem :: OS Independent",
+
+    ],
+    python_requires='>=3.6',
+)
