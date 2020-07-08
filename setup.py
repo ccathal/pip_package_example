43f58f3d3875b9369cb80485593fd5d81169c265
Cathal Corbett <cathalcorbett3@gmail.com>
2020-07-08 12:22:33 +0100

    Final version of pip package example w/ upload to PyPi

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating SYstem :: OS Independent",
+        "Operating System :: OS Independent",
 
     ],
+    license="MIT",
     python_requires='>=3.6',
 )
